3082900ed0d86a4556acc4d914328125fcac6991
Dima-Elmeev58 <147616892+Dima-Elmeev58@users.noreply.github.com>
2023-10-11 15:10:26 +0300

    Add files via upload

---
+++ Лаба1.cs
@@ -0,0 +1,17 @@
+﻿{
+    Console.WriteLine("Введите a");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите b");
+    double b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите c");
+    double c = Convert.ToDouble(Console.ReadLine());
+
+    double p = 0.5 * (a + b + c);
+
+    double h = 2 * Math.Sqrt(p * (p - a) * (p - b) * (p - c)) / a;
+
+    Console.WriteLine("Height = " + h);
+    Console.WriteLine();
+
+
+}
